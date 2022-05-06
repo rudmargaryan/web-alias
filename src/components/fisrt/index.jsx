bd547ad581b7b55f289108f67775611b2940a78a
rudmargaryan <rudikhayk2017@gmail.com>
2022-05-06 22:00:08 +0400

    l

--- src/components/fisrt/index.jsx
+++ src/components/fisrt/index.jsx
@@ -1,5 +1,5 @@
 import React,{ useState } from 'react'
-import { Link, useNavigate } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 import { clearFirstVisit } from '../../func/isFirstVisit'
 import './index.scss'
 
