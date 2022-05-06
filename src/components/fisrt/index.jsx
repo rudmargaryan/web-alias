bf37604b87377141a37e11f37bacaf6216467581
rudmargaryan <rudikhayk2017@gmail.com>
2022-05-06 21:36:55 +0400

    loader

--- src/components/fisrt/index.jsx
+++ src/components/fisrt/index.jsx
@@ -3,11 +3,18 @@ import './index.scss'
 
 export function First(){
     const [page,setPage] = useState(0)
+    function changePage(){
+        let thisPage = page
+        setPage(3)
+        setTimeout(()=>{
+            setPage(thisPage+1)
+        },2000)
+    }
     if(page === 0){
         return(
             <div className='background__schoole'>
                 <div className="background__schoole__title">HELLO...</div>
-                <button className='btn btn-next' onClick={()=>setPage(page+1)}>Next</button>
+                <button className='btn btn-next' onClick={()=>changePage()}>Next</button>
             </div>
         )
     }
@@ -15,12 +22,14 @@ export function First(){
         return(
             <div className='background__schoole'>
                 <div className="background__schoole__title">For first create teams</div>
-                <button className='btn btn-next' onClick={()=>setPage(page+1)}>Next</button>
+                <button className='btn btn-next' onClick={()=>changePage()}>Next</button>
                 <button className='btn btn-back' onClick={()=>setPage(page-1)}>Back</button>
             </div>
         )
     }
-    return(
-        <div>first</div>
-    )
+        return(
+            <div className='background__schoole'>
+                <div className="background__schoole__title">Loading...</div>
+            </div>
+        )
 }
