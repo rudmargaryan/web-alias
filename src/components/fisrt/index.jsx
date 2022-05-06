8cc86c113f7fe9fcfafbb46d648a6c9bd44637a9
rudmargaryan <rudikhayk2017@gmail.com>
2022-05-06 21:56:48 +0400

    first

--- src/components/fisrt/index.jsx
+++ src/components/fisrt/index.jsx
@@ -1,10 +1,13 @@
 import React,{ useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import { clearFirstVisit } from '../../func/isFirstVisit'
 import './index.scss'
 
 export function First(){
     const [page,setPage] = useState(0)
+    const naviagte = useNavigate()
     function changePage(pageNext){
-        setPage(3)
+        setPage('loader')
         setTimeout(()=>{
             setPage(pageNext)
         },500)
@@ -23,6 +26,23 @@ export function First(){
                 <div className="background__schoole__title">For first create teams</div>
                 <button className='btn btn-next' onClick={()=>changePage(page+1)}>Next</button>
                 <button className='btn btn-back' onClick={()=>changePage(page-1)}>Back</button>
+            </div>
+        )
+    }
+    if(page === 2){
+        return(
+            <div className='background__schoole'>
+                <div className="background__schoole__title">Second select more settings</div>
+                <button className='btn btn-next' onClick={()=>changePage(page+1)}>Next</button>
+                <button className='btn btn-back' onClick={()=>changePage(page-1)}>Back</button>
+            </div>
+        )
+    }
+    if(page === 3){
+        return(
+            <div className='background__schoole'>
+                <div className="background__schoole__title">Click to start button</div>
+                <button  className='btn btn-next' onClick={()=>clearFirstVisit(naviagte)}>{`Let's Go`}</button>
             </div>
         )
     }
--- src/func/isFirstVisit.js
+++ src/func/isFirstVisit.js
@@ -2,3 +2,9 @@ export const isFirstVisited = function(){
     let isFirst = localStorage.getItem('isFirst')
     return !!isFirst 
 }
+
+export const clearFirstVisit = function(navigate){
+    localStorage.setItem('isFirst',true)
+    navigate('/')
+    window.location.reload()
+}
